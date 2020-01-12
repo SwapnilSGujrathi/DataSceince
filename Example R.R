81be49f6d07bf101e05c88b7b83652df20935118
SwapnilSGujrathi <59788972+SwapnilSGujrathi@users.noreply.github.com>
2020-01-12 13:31:01 +0530

    Add files via upload

---
+++ Example R.R
@@ -0,0 +1,37 @@
+3^9
+25*2/5
+exp (1)
+sqrt (10)
+pi
+2*pi*6378
+2+3*4
+x <- c (2,0,0,4)
+x[1]
+x[-1]
+x[1] <-3 ; x
+x[-1] = 5 ; x
+y <- c(2,9,6,10)
+y < 9
+y [4] = 1
+y < 9
+y [y<9] = 2
+y
+
+#data frame construction
+
+df<- data.frame(x=1:5, y=c('a','b','c','d','e'))
+df1<-data.frame(x=c(1,4,9),y=c('a','b','c'))
+df1
+df[1,0]
+df[0,1]
+df[c(1),0]
+df[0,c(1)]
+df[1]
+df[2]
+df[c(1,2),2]
+df[c(3,1),1]
+df[2,c(1,2)]
+df[1,2]
+df[1,c(1,2)]
+df[3,c(1,2)]
+df[c(1:5)]
